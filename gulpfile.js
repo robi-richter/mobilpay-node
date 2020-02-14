4b84d76d03fe48eca2b51e1e0aa574599f7a362e
Robert R <robi.richter@gmail.com>
2020-02-14 13:46:17 +0200

    Build script fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var clean = require('gulp-clean');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const clean = require('gulp-clean');
 
 gulp.task('clean', function () {
-  return gulp.src('dist', {read: false})
+  return gulp.src('dist', {read: false, allowEmpty: true})
     .pipe(clean());
 });
 
-gulp.task('dist-es5', ['clean'], function() {
+gulp.task('dist-es5', gulp.series('clean', function() {
   gulp.src('lib/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist/es5/lib'));
   gulp.src('index.js')
     .pipe(babel())
     .pipe(gulp.dest('dist/es5'));
-  gulp.src(['package.json', 'README.md'])
+  return gulp.src(['package.json', 'README.md'])
     .pipe(gulp.dest('dist/es5'));
-});
+}));
